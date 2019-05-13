03f1d3b06b28a87226e8c20544ad21a4f9838555
Puja Grover <puja_grover@yahoo.com>
2019-05-13 18:52:59 -0500

    Created Director Repo & Interface

---
+++ CoderGirl_MVCMovies/Data/DirectorRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoderGirl_MVCMovies.Models;
+
+namespace CoderGirl_MVCMovies.Data
+{
+    public class DirectorRepository : IDirectorRepository
+    {
+        static List<Director> directors = new List<Director>();
+        static int nextId = 1;
+        static IDirectorRepository directorRepository = RepositoryFactory.GetDirectorRepository();
+
+        public void Delete(int id)
+        {
+           directors.RemoveAll(d => d.Id == id);
+        }
+
+        public Director GetById(int id)
+        {
+            Director director = directors.SingleOrDefault(d => d.Id == id);
+            return director;
+        }
+
+        public List<Director> GetDirectors()
+        {
+            return directors;
+        }
+
+        public int Save(Director director)
+        {
+            director.Id = nextId++;
+            directors.Add(director);
+            return director.Id;
+        }
+
+        public void Update(Director director)
+        {
+            this.Delete(director.Id);
+            directors.Add(director);
+        }
+    }
+}
